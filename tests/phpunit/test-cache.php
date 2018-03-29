4f658f6df3d3a09f389c6a0a5784fd49f957d2d9
Dima <dima92.mvs@gmail.com>
2018-03-29 14:41:59 +0300

    To clear a cache when a page update via LC interface #904

--- tests/phpunit/test-cache.php
+++ tests/phpunit/test-cache.php
@@ -54,7 +54,7 @@ class Cache extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test: Cache Disabled
+	 * Test: To clear a cache when a page update via WP Admin
 	 */
 	public function test_cache_update_page() {
 
@@ -78,6 +78,10 @@ class Cache extends WP_UnitTestCase {
 			)
 		);
 
+		// Check if page was added.
+		$page = get_post( $page_id );
+		$this->assertEquals( 'Page Test Cache', $page->post_title );
+
 		$lc_cache->set_cache( 'lc_html', $page_id, 'html' );
 		$lc_cache->set_cache( 'lc_css', $page_id, 'css' );
 		$lc_cache->set_cache( 'lc_fonts', $page_id, 'fonts' );
@@ -92,9 +96,65 @@ class Cache extends WP_UnitTestCase {
 
 		wp_update_post( wp_slash( $update_page ) );
 
+		// Check if a page was updaded.
+		$page = get_post( $page_id );
+		$this->assertEquals( 'New Content', $page->post_content );
+
 		// Check if a page is not cached.
 		$this->assertFalse( $lc_cache->cached( $page_id ) );
 
 		//fwrite( STDERR, print_r( $lc_cache->cached( $page_id ), true ) );
+	}
+
+	/**
+	 * Test: To clear a cache when a page update via LC interface
+	 */
+	public function test_cache_update_lc_interface() {
+
+		// Cache is enabled.
+		$lc_cache     = new DSLC_Cache();
+		$ref_object   = new ReflectionObject( $lc_cache );
+		$ref_property = $ref_object->getProperty( 'enabled' ); // private static $enabled.
+		$ref_property->setAccessible( true );
+		$ref_property->setValue( null, true ); // cache is enabled
+
+		$cache_enabled = $lc_cache->enabled();
+
+		// Check if cache is enabled.
+		$this->assertTrue( $cache_enabled );
+
+		$page_id = $this->factory->post->create(
+			array(
+				'post_type'    => 'page',
+				'post_title'   => 'LC Interface Cache',
+				'post_content' => 'LC Interface',
+			)
+		);
+
+		// Check if page was added.
+		$page = get_post( $page_id );
+		$this->assertEquals( 'LC Interface Cache', $page->post_title );
+
+		$lc_cache->set_cache( 'lc_html 2', $page_id, 'html' );
+		$lc_cache->set_cache( 'lc_css 2', $page_id, 'css' );
+		$lc_cache->set_cache( 'lc_fonts 2', $page_id, 'fonts' );
+
+		// Check if a page is a cached.
+		$this->assertTrue( $lc_cache->cached( $page_id ) );
+
+		/**
+		 * Add custom field ( dslc_code ). We are using the hook of add_post_meta
+		 * because in the class DSLC_Cache use the action of added_post_meta in the __construct.
+		 */
+		add_post_meta( $page_id, 'dslc_code', 'custom_dslc_code' );
+		$dslc_code = get_post_meta( $page_id, 'dslc_code', true );
+
+		// Check if custom field ( dslc_code ) was added.
+		$this->assertEquals( 'custom_dslc_code', $dslc_code );
+
+		// Check if a page is not cached.
+		$this->assertFalse( $lc_cache->cached( $page_id ) );
+
+		// fwrite( STDERR, print_r( get_transient( 'lc_cache' ), true ) );
 	}
 }
