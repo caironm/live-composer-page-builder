54340465dd1a5dc7ea1b399bbc5d68b74cca1bf9
Dima <dima92.mvs@gmail.com>
2018-03-29 11:31:35 +0300

    Test Enabled/Disabled Cache ( Second method ) #904

--- tests/phpunit/test-cache.php
+++ tests/phpunit/test-cache.php
@@ -11,52 +11,45 @@
 class Cache extends WP_UnitTestCase {
 
 	/**
-	 * Test: Cache Enabled
+	 * Test: Cache Enabled/Disabled
 	 */
-	public function test_cache_enabled() {
+	public function test_cache_enabled_or_disabled() {
 
-		$expected = array(
-			'lc_caching_engine' => 'enabled',
-		);
+		/**
+		 * Cache is enabled
+		 */
 
-		// Enable a cache.
-		update_option( 'dslc_plugin_options', $expected );
-		$dslc_plugin_options = get_option( 'dslc_plugin_options' );
-		$dslc_cache = $dslc_plugin_options['lc_caching_engine'];
+		$lc_cache     = new DSLC_Cache();
+		$ref_object   = new ReflectionObject( $lc_cache );
+		$ref_property = $ref_object->getProperty( 'enabled' ); // private static $enabled.
+		$ref_property->setAccessible( true );
+		$ref_property->setValue( null, true ); // cache is enabled
 
-		// Add a custom transient cache in the DB.
-		set_transient( 'lc_cache', 'lc_custom_cache' );
-		$transient_lc_cache = get_transient( 'lc_cache' );
+		$cache_enabled = $lc_cache->enabled();
+		$lc_cache->set_cache( 'lc_custom_cache', '5', 'html' );
+		$transient_lc_cache = $lc_cache->get_cache( '5', 'html' );
 
 		// Check if cache is enabled.
-		$this->assertEquals( 'enabled', $dslc_cache );
+		$this->assertTrue( $cache_enabled );
 
 		// Check if have a transient cache in the DB.
 		$this->assertEquals( 'lc_custom_cache', $transient_lc_cache );
-	}
 
-	/**
-	 * Test: Cache Disabled
-	 */
-	public function test_cache_disabled() {
+		/**
+		 * Cache is disabled
+		 */
 
-		$expected = array(
-			'lc_caching_engine' => 'disabled',
-		);
-
-		// Disable a cache.
-		update_option( 'dslc_plugin_options', $expected );
-		$dslc_plugin_options = get_option( 'dslc_plugin_options' );
-		$dslc_cache = $dslc_plugin_options['lc_caching_engine'];
-
-		// Add a custom transient cache in the DB.
-		set_transient( 'lc_cache', 'lc_custom_cache' );
-		$transient_lc_cache = get_transient( 'lc_cache' );
+		$ref_property->setValue( null, false ); // cache is disabled
+		$cache_disabled = $lc_cache->enabled();
+		$lc_cache->delete_cache();
+		$transient_lc_cache = $lc_cache->get_cache( '5', 'html' );
 
 		// Check if cache is disabled.
-		$this->assertEquals( 'disabled', $dslc_cache );
+		$this->assertFalse( $cache_disabled );
 
 		// Check if does not have a transient cache in the DB.
-		$this->assertTrue( delete_transient( 'lc_cache' ) );
+		$this->assertEmpty( $transient_lc_cache );
+
+		// fwrite( STDERR, print_r( $transient_lc_cache, true ) );
 	}
 }
